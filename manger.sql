4ffc14f6768d57b49ba1fd30a2124cae7d976c01
unknown <C542934hen@outlook.com>
2023-02-10 10:47:52 +0800

    1

---
+++ manger.sql
@@ -0,0 +1,4 @@
+INSERT INTO `manger` VALUES ('1', '张三', '一年一班', '0001');
+INSERT INTO `manger` VALUES ('2', '李四', '一年二班', '0002');
+INSERT INTO `manger` VALUES ('3', '王五', '一年三班', '0003');
+INSERT INTO `manger` VALUES ('4', '周杰伦', '三年二班', '0004');
